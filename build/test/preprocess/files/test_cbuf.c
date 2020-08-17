3c6e4bede29bafbaf476c7e0eeb55d265e48b046
hung vu <viethungvu@hotmail.com>
2020-08-17 11:36:23 +0200

    add ceedling test settings

---
+++ build/test/preprocess/files/test_cbuf.c
@@ -0,0 +1,30 @@
+#include "build/temp/_test_cbuf.c"
+#include "src/cbuf.h"
+#include "C:/Ruby27-x64/lib/ruby/gems/2.7.0/gems/ceedling-0.30.0/vendor/unity/src/unity.h"
+
+
+
+
+void setUp(void)
+
+{
+
+}
+
+
+
+void tearDown(void)
+
+{
+
+}
+
+
+
+void test_cbuf_NeedToImplement(void)
+
+{
+
+    UnityIgnore( (("Need to Implement cbuf")), (UNITY_UINT)(15));
+
+}
---
+++ test/test_cbuf.c
@@ -0,0 +1,16 @@
+#include "unity.h"
+
+#include "cbuf.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_cbuf_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement cbuf");
+}
